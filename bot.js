36f44d23675deb979b09ee31d1555cac23ab8f09
Terrerox <terrero0611@gmail.com>
2021-01-06 12:27:23 -0500

    wikipedia searcher

--- bot.js
+++ bot.js
@@ -1,31 +1,48 @@
-import dotenv from 'dotenv'
-import Telegraf from 'telegraf'
+import axios from "axios";
+import dotenv from "dotenv";
+import Telegraf from "telegraf";
 dotenv.config();
 
-const bot = new Telegraf(process.env.LOCAL_TOKEN)
+const bot = new Telegraf(process.env.LOCAL_TOKEN);
 
 bot.start((ctx) => {
-    ctx.reply(`Welcome ${ctx.from.first_name} ${ctx.from.last_name}`)
-})
+  ctx.reply(`Welcome ${ctx.from.first_name} ${ctx.from.last_name}`);
+});
 
 bot.settings((ctx) => {
-    ctx.reply('settings')
-})
+  ctx.reply("settings");
+});
 
-bot.command(['mycommand','mycommand2'], (ctx) => {
-    ctx.reply('Testing with different commands')
-})
+bot.command(["mycommand", "mycommand2"], (ctx) => {
+  ctx.reply("Testing with different commands");
+});
 
-bot.hears('say hi', (ctx) => {
-    ctx.reply('Hi')
-})
+bot.hears("say hi", (ctx) => {
+  ctx.reply("Hi");
+});
 
-bot.on('text', ctx => {
-    ctx.reply('you texting something')
-})
-
-bot.on('sticker', ctx => {
-    ctx.reply('you like stickers!')
-})
-
-bot.launch()
+bot.command("/^/heya/", (ctx) => {
+  ctx.reply(`mmg`);
+});
+bot.command("/echo", ({ reply, message }) => {
+  const msg = message.text.replace("/echo", "");
+  reply(msg);
+});
+bot.command("/wk", ({ reply, message }) => {
+  const search = message.text.replace("/wk", "");
+  let url = encodeURI(
+      `https://es.wikipedia.org/api/rest_v1/page/summary/${search}`
+    ),
+    extract;
+  const getWikiInfo = async url => {
+    try {
+      const info = await axios.get(url);
+      extract = info.data.extract;
+      reply(`Your result:\n\n${extract}`)
+    } catch (e) {
+      reply('We have a problem, try to search again') 
+    }
+  };
+  getWikiInfo(url);
+});
+bot.launch();
