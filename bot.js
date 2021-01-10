8803ae7e5dcfa2f9dc1c299bf72ae95834318541
Terrerox <terrero0611@gmail.com>
2021-01-10 13:03:52 -0500

    img searcher

--- bot.js
+++ bot.js
@@ -25,21 +25,44 @@ bot.command("/echo", ({ reply, message }) => {
   const msg = message.text.replace("/echo", "");
   reply(msg);
 });
+
+//wikipedia searcher
 bot.command("/wk", ({ reply, message }) => {
   const search = message.text.replace("/wk", "");
   let url = encodeURI(
       `https://es.wikipedia.org/api/rest_v1/page/summary/${search}`
     ),
     extract;
-  const getWikiInfo = async url => {
+  const getWikiInfo = async (url) => {
     try {
       const info = await axios.get(url);
       extract = info.data.extract;
-      reply(`Your result:\n\n${extract}`)
+      reply(`Your result:\n\n${extract}`);
     } catch (e) {
-      reply('I had a problem, try to search again') 
+      reply("I've had a problem, try to search again");
     }
   };
   getWikiInfo(url);
+});
+
+//image searcher
+bot.command("/img", ({ reply, message }) => {
+  const img = message.text.replace("/img", "");
+
+  let payload = {
+    method: "GET",
+    url: "https://bing-image-search1.p.rapidapi.com/images/search",
+    params: { q: img, count : '1'},
+    headers: {
+      "x-rapidapi-key": "cf6eb456a5mshc2e5d3b18ada1c3p1a0dedjsncdfaab08b0a5",
+      "x-rapidapi-host": "bing-image-search1.p.rapidapi.com",
+    },
+  };
+  const getImg = async (payload) => {
+    let info = await axios.request(payload),
+      image = info.data.value[0].contentUrl;
+    reply(`Your result:\n ${image}`);
+  };
+  getImg(payload);
 });
 bot.launch();
